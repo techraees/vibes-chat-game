2061f0948d0f8cfad589428cf8640dcca19959a1
d4ne <danielgawenda92@icloud.com>
2024-04-12 22:31:13 +0200

    Create mongoose connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectToDatabase from "./database/connect";
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.listen(PORT, () => {
+    connectToDatabase();
     console.log(`Server is running on port ${PORT}`);
 });
