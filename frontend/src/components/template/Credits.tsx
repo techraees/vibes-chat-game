9c20813f607772e99a1f6b5a12f6be2fe722778f
d4ne <danielgawenda92@icloud.com>
2024-04-13 21:21:19 +0200

    Add tsx files for views

--- frontend/src/components/template/Credits.tsx
+++ frontend/src/components/template/Credits.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from '@/store'
 import Tag from '@/components/ui/Tag'
 import { HiFire, HiOutlineTag } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 
 const Credits = () => {
     const { vCard, vCoins } = useAppSelector((state) => state.auth.user)
@@ -8,13 +9,15 @@ const Credits = () => {
     return (
         <div className="flex">
             <div className="mr-2 rtl:ml-2">
-                <Tag
-                    prefix={
-                        <HiFire className="text-base text-grey-500 mr-1 rtl:ml-1" />
-                    }
-                >
-                    {vCard} credits
-                </Tag>
+                <Link to="purchase">
+                    <Tag
+                        prefix={
+                            <HiFire className="text-base text-grey-500 mr-1 rtl:ml-1" />
+                        }
+                    >
+                        {vCard} credits
+                    </Tag>
+                </Link>
             </div>
             <div className="mr-2 rtl:ml-2">
                 <Tag
--- frontend/src/configs/routes.config/routes.config.ts
+++ frontend/src/configs/routes.config/routes.config.ts
@@ -1,6 +1,7 @@
 import { lazy } from 'react'
 import authRoute from './authRoute'
 import type { Routes } from '@/@types/routes'
+import path from 'path'
 
 export const publicRoutes: Routes = [...authRoute]
 
@@ -9,6 +10,54 @@ export const protectedRoutes = [
         key: 'home',
         path: '/home',
         component: lazy(() => import('@/views/Home')),
-        authority: [],
-    }
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'feed',
+        path: '/feed',
+        component: lazy(() => import('@/views/Feed')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'inventory',
+        path: '/inventory',
+        component: lazy(() => import('@/views/Inventory')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'shop',
+        path: '/shop',
+        component: lazy(() => import('@/views/Shop')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'chat',
+        path: '/chat',
+        component: lazy(() => import('@/views/Chat')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'purchase',
+        path: '/purchase',
+        component: lazy(() => import('@/views/Purchase')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'profile',
+        path: '/profile',
+        component: lazy(() => import('@/views/Profile')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'settings',
+        path: '/settings',
+        component: lazy(() => import('@/views/Settings')),
+        authority: ['user', 'admin'],
+    },
+    {
+        key: 'help',
+        path: '/help',
+        component: lazy(() => import('@/views/Help')),
+        authority: ['user', 'admin'],
+    },
 ]
