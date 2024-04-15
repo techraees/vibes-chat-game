ade6cc244b4e17640dca21cd45e3741ac031dcf4
d4ne <danielgawenda92@icloud.com>
2024-04-15 12:46:56 +0200

    Add comments

--- frontend/src/components/template/Canvas.tsx
+++ frontend/src/components/template/Canvas.tsx
@@ -1,19 +1,30 @@
 import { useRef, useEffect } from 'react'
 import { NAV_ITEM_HEIGHT } from '@/constants/theme.constant'
 
+// Canvas props
 interface CanvasProps {
     width: number
     height: number
 }
 
+// Canvas component for chat page
 const Canvas = ({ width, height }: CanvasProps) => {
+    // Canvas reference
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
     useEffect(() => {
+        // Draw a blue rectangle on the canvas
         if (canvasRef.current) {
             const canvas = canvasRef.current
             const context = canvas.getContext('2d')
 
+            // Disable right click
+            canvas.oncontextmenu = (e) => {
+                e.preventDefault()
+                e.stopPropagation()
+            }
+
+            // Draw a blue rectangle
             if (context) {
                 context.fillStyle = 'blue'
                 context.fillRect(0, 0, canvas.width, canvas.height)
@@ -21,9 +32,11 @@ const Canvas = ({ width, height }: CanvasProps) => {
         }
     }, [])
 
+    // Return canvas
     return <canvas ref={canvasRef} height={height} width={width} />
 }
 
+// Default props
 Canvas.defaultProps = {
     width: window.innerWidth,
     height: window.innerHeight - NAV_ITEM_HEIGHT,
