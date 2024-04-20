507d8a52b5ad93deb9da6591950a0be4176009df
d4ne <danielgawenda92@icloud.com>
2024-04-20 11:38:23 +0200

    Cleanup

--- backend/src/controllers/verify.controller.ts
+++ backend/src/controllers/verify.controller.ts
@@ -5,11 +5,10 @@ import User from "../models/user.model";
 export const verify = async (req: express.Request, res: express.Response) => {
     try {
         const { username } = req.body;
-        const token: string = req.cookies.authToken;
+        const token: string | undefined = req.cookies.authToken;
 
         if (!token) {
-            res.status(401).json({ id: null });
-            return;
+            return res.status(401).json({ id: null });
         }
 
         const decoded: any = jwt.verify(
@@ -18,27 +17,18 @@ export const verify = async (req: express.Request, res: express.Response) => {
         );
 
         if (!decoded) {
-            res.status(401).json({ id: null });
-            return;
+            return res.status(401).json({ id: null });
         }
 
         const user = await User.findById(decoded.userId).select("-password");
 
-        if (!user) {
-            res.status(401).json({ id: null });
-            return;
+        if (!user || user.username !== username) {
+            return res.status(401).json({ id: null });
         }
 
-        if (user?.username !== username) {
-            res.status(401).json({ id: null });
-            return;
-        }
-
-        if (user?.username === username) {
-            res.status(201).json({ id: user?._id });
-        }
+        return res.status(201).json({ id: user._id });
     } catch (error) {
-        console.log(`Error in verify controller: ${error}`);
+        console.error(`Error in verify controller: ${error}`);
         return res.status(500).json({ message: "Something went wrong" });
     }
 };
